ebb61427fcd5dfe1cee51385104e4c1032a51a83
Juha <kukiyomenaisapp@gmail.com>
2023-01-24 22:44:29 +0900

    ウィンドウ生成ボタン作成

---
+++ Assets/Dock/Editor/Dock.cs
@@ -0,0 +1,14 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace JuhaKurisu.UnityDock
+{
+    public class DockWindow : EditorWindow
+    {
+        [MenuItem("Tools/Dock")]
+        private static void Create()
+        {
+
+        }
+    }
+}
