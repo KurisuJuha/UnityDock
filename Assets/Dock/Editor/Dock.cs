3e6be9cb9f81ddcf4653d916b0018658eca4ce9e
KYapp <kukiyomenaisapp@gmail.com>
2023-01-24 23:06:50 +0900

    タブによって表示される内容を関数に分けた

--- Assets/Dock/Editor/Dock.cs
+++ Assets/Dock/Editor/Dock.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEditor;
 using UnityEngine;
 
@@ -5,8 +6,39 @@ namespace JuhaKurisu.UnityDock
 {
     public class DockWindow : EditorWindow
     {
+        private static DockWindow dockWindow;
+        private readonly string[] tabContents = { "Packages", "ColorScheme" };
+        private readonly Action[] tabOnGUIFuncs;
+
+        public DockWindow()
+        {
+            tabOnGUIFuncs = new Action[] { PackagesTab, ColorSchemeTab };
+        }
+
+        int tabIndex;
+
         [MenuItem("Tools/Dock")]
         private static void Create()
+        {
+            if (dockWindow != null) return;
+            dockWindow = GetWindow<DockWindow>("Dock");
+        }
+
+        private void OnGUI()
+        {
+            using (new EditorGUILayout.HorizontalScope(EditorStyles.toolbar))
+            {
+                tabIndex = GUILayout.Toolbar(tabIndex, tabContents, new GUIStyle(EditorStyles.toolbarButton), GUI.ToolbarButtonSize.Fixed);
+            }
+            tabOnGUIFuncs[tabIndex].Invoke();
+        }
+
+        private void PackagesTab()
+        {
+
+        }
+
+        private void ColorSchemeTab()
         {
 
         }
